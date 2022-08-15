81dc8801e23720f69f91ec7835d4bff13f908cac
zaidalyafeai <alyafey22@gmail.com>
2022-08-15 09:39:12 +0300

    add ip

--- inference/server_test.py
+++ inference/server_test.py
@@ -12,5 +12,5 @@ def uploader_file():
       f.save('video.mp4')
       return 'file uploaded successfully'
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(host='0.0.0.0' , port=5000)
    
