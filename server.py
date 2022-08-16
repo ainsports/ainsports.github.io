d67331ecf8385351209079b2d3974a1dbf0cc8ef
zaidalyafeai <alyafey22@gmail.com>
2022-08-16 11:37:14 +0300

    end test, roll back

--- server.py
+++ server.py
@@ -17,11 +17,10 @@ def upload_file():
 def recieve():
     if request.method == 'POST':
         print("recieved POST")
-        # f = request.files['file']
-        # f.save('video.mp4')
+        f = request.files['file']
+        f.save('video.mp4')
         if args.CPU:
             print('prediction is done on CPU')
-        # dataset, model = initialize_model(args) 
-        # json_data = run_test(args, model, dataset)
-        # return json_data
-        return jsonify({"hi": "there"})
+        dataset, model = initialize_model(args) 
+        json_data = run_test(args, model, dataset)
+        return json_data
