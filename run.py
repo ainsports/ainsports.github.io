73848802291e060451a058766a1e0cd283d80e19
zaidalyafeai <alyafey22@gmail.com>
2022-08-16 09:37:12 +0300

    move files to main dir

---
+++ run.py
@@ -0,0 +1,39 @@
+import os
+import logging
+from datetime import datetime
+import time
+import numpy as np
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+import torch
+
+from dataset import SoccerNetClipsTesting
+from model import ContextAwareModel
+from train import test
+
+# Fixing seeds for reproducibility
+torch.manual_seed(0)
+np.random.seed(0)
+
+def initialize_model(args):
+    dataset_Test = SoccerNetClipsTesting(path=args.video_path, features=args.features, framerate=args.framerate, chunk_size=args.chunk_size*args.framerate, receptive_field=args.receptive_field*args.framerate)
+    # Create the deep learning model
+    model = ContextAwareModel(weights=args.load_weights, input_size=args.num_features, num_classes=dataset_Test.num_classes, chunk_size=args.chunk_size*args.framerate, dim_capsule=args.dim_capsule, receptive_field=args.receptive_field*args.framerate, num_detections=dataset_Test.num_detections, framerate=args.framerate)
+    if not args.CPU:
+        model = model.cuda()
+    
+    # Load the best model and compute its performance
+    checkpoint = torch.load(os.path.join("models", args.model_name, "model.pth.tar"))
+    model.load_state_dict(checkpoint['state_dict'])
+    return model
+
+def run_test(args, model):
+    dataset_Test = SoccerNetClipsTesting(path=args.video_path, features=args.features, framerate=args.framerate, chunk_size=args.chunk_size*args.framerate, receptive_field=args.receptive_field*args.framerate)
+    test_loader = torch.utils.data.DataLoader(dataset_Test,
+        batch_size=1, shuffle=False,
+        num_workers=1, pin_memory=True)
+    return test(test_loader, model=model, model_name=args.model_name, save_predictions=Trueh, cpu = args.CPU)
+
+
+
+
---
+++ server_test.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploader_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save('video.mp4')
+      return 'file uploaded successfully'
+if __name__ == '__main__':
+   app.run(debug = True)
+   
